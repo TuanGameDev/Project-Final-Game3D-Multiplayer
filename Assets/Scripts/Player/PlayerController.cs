12c461aeccef2a0eb59356a5fa8b0b84979324c9
TuanGameDev <hoangtuan18203@gmail.com>
2024-01-08 01:48:35 +0700

    .

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,7 +15,33 @@ public class PlayerController : MonoBehaviourPun
     public Animator playerAnim;
     public Rigidbody rb;
     [Header("Player HUD")]
+    [SerializeField] private float _walkSpeed = 1.0f;
+    [SerializeField] private float _runSpeed = 4.5f;
+    [SerializeField] private float _jumpSpeed = 7.5f;
+    [SerializeField] private float _stickToGroundForce = 5.0f;
+    [SerializeField] private float _gravityMultiplier = 2.5f;
+    // Use Standard Assets Mouse Look class for mouse input -> Camera Look Control
+    [SerializeField] private UnityStandardAssets.Characters.FirstPerson.MouseLook _mouseLook = new UnityStandardAssets.Characters.FirstPerson.MouseLook();
 
+    // Private internals
+    private Camera _camera = null;
+    private bool _jumpButtonPressed = false;
+    private Vector2 _inputVector = Vector2.zero;
+    private Vector3 _moveDirection = Vector3.zero;
+    private bool _previouslyGrounded = false;
+    private bool _isWalking = true;
+    private bool _isJumping = false;
+
+    // Timers
+    private float _fallingTimer = 0.0f;
+
+    private CharacterController _characterController = null;
+    private PlayerMoveStatus _movementStatus = PlayerMoveStatus.NotMoving;
+
+    // Public Properties
+    public PlayerMoveStatus movementStatus { get { return _movementStatus; } }
+    public float walkSpeed { get { return _walkSpeed; } }
+    public float runSpeed { get { return _runSpeed; } }
     //
     public int id;
     public int currentHP;
@@ -45,6 +71,119 @@ public class PlayerController : MonoBehaviourPun
             me = this;
         else
             rb.isKinematic = false;
+    }
+    protected void Start()
+    {
+        // Cache component references
+        _characterController = GetComponent<CharacterController>();
+
+        // Get the main camera and cache local position within the FPS rig 
+        _camera = Camera.main;
+
+        // Set initial to not jumping and not moving
+        _movementStatus = PlayerMoveStatus.NotMoving;
+
+        // Reset timers
+        _fallingTimer = 0.0f;
+
+        // Setup Mouse Look Script
+        _mouseLook.Init(transform, _camera.transform);
+    }
+
+    protected void Update()
+    {
+        // If we are falling increment timer
+        if (_characterController.isGrounded) _fallingTimer = 0.0f;
+        else _fallingTimer += Time.deltaTime;
+
+        // Allow Mouse Look a chance to process mouse and rotate camera
+        if (Time.timeScale > Mathf.Epsilon)
+            _mouseLook.LookRotation(transform, _camera.transform);
+
+        // Process the Jump Button
+        // the jump state needs to read here to make sure it is not missed
+        if (!_jumpButtonPressed)
+            _jumpButtonPressed = Input.GetButtonDown("Jump");
+
+        // Calculate Character Status
+        if (!_previouslyGrounded && _characterController.isGrounded)
+        {
+            if (_fallingTimer > 0.5f)
+            {
+                // TODO: Play Landing Sound
+            }
+
+            _moveDirection.y = 0f;
+            _isJumping = false;
+            _movementStatus = PlayerMoveStatus.Landing;
+        }
+        else
+            if (!_characterController.isGrounded)
+            _movementStatus = PlayerMoveStatus.NotGrounded;
+        else
+            if (_characterController.velocity.sqrMagnitude < 0.01f)
+            _movementStatus = PlayerMoveStatus.NotMoving;
+        else
+            if (_isWalking)
+            _movementStatus = PlayerMoveStatus.Walking;
+        else
+            _movementStatus = PlayerMoveStatus.Running;
+
+        _previouslyGrounded = _characterController.isGrounded;
+    }
+
+    protected void FixedUpdate()
+    {
+        // Read input from axis
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool waswalking = _isWalking;
+        _isWalking = !Input.GetKey(KeyCode.LeftShift);
+
+        // Set the desired speed to be either our walking speed or our running speed
+        float speed = _isWalking ? _walkSpeed : _runSpeed;
+        _inputVector = new Vector2(horizontal, vertical);
+
+        // normalize input if it exceeds 1 in combined length:
+        if (_inputVector.sqrMagnitude > 1) _inputVector.Normalize();
+
+        // Always move along the camera forward as it is the direction that it being aimed at
+        Vector3 desiredMove = transform.forward * _inputVector.y + transform.right * _inputVector.x;
+
+        // Get a normal for the surface that is being touched to move along it
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(transform.position, _characterController.radius, Vector3.down, out hitInfo, _characterController.height / 2f, 1))
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+        // Scale movement by our current speed (walking value or running value)
+        _moveDirection.x = desiredMove.x * speed;
+        _moveDirection.z = desiredMove.z * speed;
+
+        // If grounded
+        if (_characterController.isGrounded)
+        {
+            // Apply severe down force to keep control sticking to floor
+            _moveDirection.y = -_stickToGroundForce;
+
+            // If the jump button was pressed then apply speed in up direction
+            // and set isJumping to true. Also, reset jump button status
+            if (_jumpButtonPressed)
+            {
+                _moveDirection.y = _jumpSpeed;
+                _jumpButtonPressed = false;
+                _isJumping = true;
+                // TODO: Play Jumping Sound
+            }
+        }
+        else
+        {
+            // Otherwise we are not on the ground so apply standard system gravity multiplied
+            // by our gravity modifier
+            _moveDirection += Physics.gravity * _gravityMultiplier * Time.fixedDeltaTime;
+        }
+
+        // Move the Character Controller
+        _characterController.Move(_moveDirection * Time.fixedDeltaTime);
     }
     [PunRPC]
     public void TakeDamage(int damageAmount)
