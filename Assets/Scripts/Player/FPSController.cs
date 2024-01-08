702ce654bb678e3634685f5460e92555b62a7476
TuanGameDev <hoangtuan18203@gmail.com>
2024-01-09 06:44:35 +0700

    .

--- Assets/Scripts/Player/FPSController.cs
+++ Assets/Scripts/Player/FPSController.cs
@@ -3,12 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
-
-// Enumerations
 public enum  PlayerMoveStatus 				{ NotMoving, Crouching, Walking, Running, NotGrounded, Landing }
 public enum  CurveControlledBobCallbackType { Horizontal, Vertical }
-
-// Delegates
 public delegate void CurveControlledBobCallback ();
 
 [System.Serializable]
@@ -25,30 +21,16 @@ public class CurveControlledBob
 	[SerializeField] AnimationCurve _bobcurve = new AnimationCurve(	new Keyframe(0f, 0f), new Keyframe(0.5f, 1f),
 																	new Keyframe(1f, 0f), new Keyframe(1.5f, -1f),
 																	new Keyframe(2f, 0f)); 
-
-	// Inspector Assigned Bob Control Variables
 	[SerializeField] float 			_horizontalMultiplier	 			= 0.01f;
 	[SerializeField] float 			_verticalMultiplier 				= 0.02f;
 	[SerializeField] float 			_verticaltoHorizontalSpeedRatio 	= 2.0f;
 	[SerializeField] float 			_baseInterval						= 1.0f;
-
-	// Internals
 	private float _prevXPlayHead;
 	private float _prevYPlayHead;
 	private float _xPlayHead;
 	private float _yPlayHead;
 	private float _curveEndTime;
 	private List<CurveControlledBobEvent> _events = new List<CurveControlledBobEvent> ();
-
-	public void Initialize()
-	{
-		// Record time length of bob curve
-		_curveEndTime 	= _bobcurve[_bobcurve.length - 1].time;
-		_xPlayHead 		= 0.0f;
-		_yPlayHead 		= 0.0f;
-		_prevXPlayHead	= 0.0f;
-		_prevYPlayHead	= 0.0f;
-	}
 
 	public void RegisterEventCallback( float time, CurveControlledBobCallback function, CurveControlledBobCallbackType type )
 	{
@@ -64,7 +46,6 @@ public class CurveControlledBob
 			}
 		);
 	}
-
 	public Vector3 GetVectorOffset(float speed)
 	{
 		_xPlayHead += (speed*Time.deltaTime)/_baseInterval;
@@ -75,8 +56,6 @@ public class CurveControlledBob
 
 		if ( _yPlayHead > _curveEndTime)
 			_yPlayHead -= _curveEndTime;
-
-		// Process Events
 		for (int i = 0; i < _events.Count; i++) 
 		{
 			CurveControlledBobEvent ev = _events [i];
@@ -117,8 +96,6 @@ public class FPSController : MonoBehaviour
 {
 	public List<AudioSource> AudioSources = new List<AudioSource>();
 	private int _audioToUse = 0;
-
-	// Inspector Assigned Locomotion Settings
 	[SerializeField] private float 	_walkSpeed			= 2.0f;
 	[SerializeField] private float 	_runSpeed			= 4.5f;
 	[SerializeField] private float 	_jumpSpeed			= 7.5f;
@@ -128,11 +105,7 @@ public class FPSController : MonoBehaviour
 	[SerializeField] private float  _runStepLengthen	= 0.75f;
 	[SerializeField] private CurveControlledBob _headBob= new CurveControlledBob();
 	[SerializeField] private GameObject _flashLight 	= null;
-
-	// Use Standard Assets Mouse Look class for mouse input -> Camera Look Control
 	[SerializeField] private UnityStandardAssets.Characters.FirstPerson.MouseLook _mouseLook = new UnityStandardAssets.Characters.FirstPerson.MouseLook();
-
-	// Private internals
 	private Camera 		_camera							= null;
 	private bool 		_jumpButtonPressed 				= false;
 	private Vector2 	_inputVector					= Vector2.zero;
@@ -143,14 +116,11 @@ public class FPSController : MonoBehaviour
 	private bool		_isCrouching					= false;
 	private Vector3		_localSpaceCameraPos			= Vector3.zero;
 	private float		_controllerHeight				= 0.0f;
-
-	// Timers
 	private float 		_fallingTimer 					= 0.0f;
 
 	private CharacterController _characterController	= null;
 	private PlayerMoveStatus	_movementStatus 		=	PlayerMoveStatus.NotMoving;
 
-	// Public Properties
 	public PlayerMoveStatus movementStatus	{ get{ return _movementStatus; }}
 	public float walkSpeed	 				{ get{ return _walkSpeed;}}
 	public float runSpeed  					{ get{ return _runSpeed;}}
@@ -168,25 +138,13 @@ public class FPSController : MonoBehaviour
     }
     protected void Start()
 	{
-		// Cache component references
 		_characterController = GetComponent<CharacterController> ();
 		_controllerHeight = _characterController.height;
-
-		// Get the main camera and cache local position within the FPS rig 
 		_camera = Camera.main;
 		_localSpaceCameraPos = _camera.transform.localPosition;
-
-		// Set initial to not jumping and not moving
 		_movementStatus = PlayerMoveStatus.NotMoving;
-
-		// Reset timers
 		_fallingTimer 			= 0.0f;
-
-		// Setup Mouse Look Script
 		_mouseLook.Init(transform , _camera.transform);
-
-		// Initiate Head Bob Object
-		_headBob.Initialize();
 		_headBob.RegisterEventCallback (1.5f, PlayFootStepSound, CurveControlledBobCallbackType.Vertical); 
 
 		if (_flashLight)
@@ -195,11 +153,8 @@ public class FPSController : MonoBehaviour
 
 	protected void Update()
 	{
-		// If we are falling increment timer
 		if (_characterController.isGrounded)  _fallingTimer=0.0f;
 		else 								  _fallingTimer+=Time.deltaTime;
-
-		// Allow Mouse Look a chance to process mouse and rotate camera
 		if (Time.timeScale>Mathf.Epsilon)
 			_mouseLook.LookRotation (transform, _camera.transform);
 
@@ -208,9 +163,6 @@ public class FPSController : MonoBehaviour
 			if (_flashLight)
 				_flashLight.SetActive (!_flashLight.activeSelf);
 		}
-
-		// Process the Jump Button
-		// the jump state needs to read here to make sure it is not missed
 		if (!_jumpButtonPressed && !_isCrouching)
 			_jumpButtonPressed = Input.GetButtonDown("Jump");
 
@@ -219,8 +171,6 @@ public class FPSController : MonoBehaviour
 			_isCrouching = !_isCrouching;
 			_characterController.height = _isCrouching==true?_controllerHeight / 2.0f: _controllerHeight;
 		}
-
-		// Calculate Chatacter Status
 		if (!_previouslyGrounded && _characterController.isGrounded) 
 		{
 			if (_fallingTimer > 0.5f) 
@@ -254,39 +204,22 @@ public class FPSController : MonoBehaviour
 
 	protected void FixedUpdate()
 	{
-		// Read input from axis
 		float horizontal	= Input.GetAxis("Horizontal");
 		float vertical 		= Input.GetAxis("Vertical");
 		bool  waswalking	= _isWalking;
 		_isWalking 			= !Input.GetKey(KeyCode.LeftShift);
-
-		// Set the desired speed to be either our walking speed or our running speed
 		float speed = _isCrouching ? _crouchSpeed :_isWalking ? _walkSpeed : _runSpeed;
 		_inputVector = new Vector2(horizontal, vertical);
-
-		// normalize input if it exceeds 1 in combined length:
 		if (_inputVector.sqrMagnitude > 1)	_inputVector.Normalize();
-
-		// Always move along the camera forward as it is the direction that it being aimed at
 		Vector3 desiredMove = transform.forward*_inputVector.y + transform.right*_inputVector.x;
-
-		// Get a normal for the surface that is being touched to move along it
 		RaycastHit hitInfo;
 		if (Physics.SphereCast (transform.position, _characterController.radius, Vector3.down, out hitInfo, _characterController.height / 2f, 1))
 			desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
-
-		// Scale movement by our current speed (walking value or running value)
 		_moveDirection.x = desiredMove.x*speed;
 		_moveDirection.z = desiredMove.z*speed;
-
-		// If grounded
 		if (_characterController.isGrounded)
 		{
-			// Apply severe down force to keep control sticking to floor
 			_moveDirection.y = -_stickToGroundForce;
-
-			// If the jump button was pressed then apply speed in up direction
-			// and set isJumping to true. Also, reset jump button status
 			if (_jumpButtonPressed)
 			{
 				_moveDirection.y 	= _jumpSpeed;
@@ -297,15 +230,9 @@ public class FPSController : MonoBehaviour
 		}
 		else
 		{
-			// Otherwise we are not on the ground so apply standard system gravity multiplied
-			// by our gravity modifier
 			_moveDirection += Physics.gravity*_gravityMultiplier*Time.fixedDeltaTime;
 		}
-
-		// Move the Character Controller
 		_characterController.Move(_moveDirection*Time.fixedDeltaTime);
-
-		// Are we moving
 		Vector3 speedXZ = new Vector3( _characterController.velocity.x, 0.0f, _characterController.velocity.z);
 		if ( speedXZ.magnitude > 0.01f)
 			_camera.transform.localPosition = _localSpaceCameraPos + _headBob.GetVectorOffset (  speedXZ.magnitude * ( _isCrouching||_isWalking?1.0f:_runStepLengthen));
