4999dde263e33edf968683cb8f18f5c0c5a5fd3c
TuanGameDev <hoangtuan18203@gmail.com>
2024-01-12 01:36:24 +0700

    .

--- Assets/Scripts/Zombie/AI Zombie.cs
+++ Assets/Scripts/Zombie/AI Zombie.cs
@@ -1,18 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
-public class AIZombie : MonoBehaviour
+public class AIZombie : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Enemy Status")]
+    public string dead = "Death";
+    public int damage;
+    public string enemyName;
+    public float moveSpeed;
+    public int currentHP;
+    public int maxHP;
+    public float chaseRange;
+    public float attackRange;
+    public float playerdetectRate;
+    private float lastPlayerDetectTime;
+    public float attackrate;
+    private float lastattackTime;
+    public Rigidbody rb;
+    private FPSController[] playerInScene;
+    private FPSController targetPlayer;
+    private float maxHealthValue;
+    public int curAttackerID;
+    private void Update()
     {
-        
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+        if (targetPlayer != null)
+        {
+            float dist = Vector2.Distance(transform.position, targetPlayer.transform.position);
+            float face = targetPlayer.transform.position.x - transform.position.x;
+
+            if (face > 0)
+            {
+               // photonView.RPC("FlipLeft", RpcTarget.All);
+            }
+            else
+            {
+               // photonView.RPC("FlipRight", RpcTarget.All);
+            }
+
+            if (dist < attackRange && Time.time - lastattackTime >= attackrate)
+            {
+                Attack();
+            }
+            else if (dist > attackRange)
+            {
+                Vector3 dir = targetPlayer.transform.position - transform.position;
+                rb.velocity = dir.normalized * moveSpeed;
+               // aim.SetBool("Move", true);
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+               // aim.SetBool("Move", false);
+            }
+        }
+
+        DetectPlayer();
+    }
+    void Attack()
+    {
+      //  aim.SetTrigger("Attack");
+        lastattackTime = Time.time;
+        targetPlayer.photonView.RPC("TakeDamage", RpcTarget.All, damage);
     }
 
-    // Update is called once per frame
-    void Update()
+    void DetectPlayer()
     {
-        
+        if (Time.time - lastPlayerDetectTime > playerdetectRate)
+        {
+            lastPlayerDetectTime = Time.time;
+            playerInScene = FindObjectsOfType<FPSController>();
+            foreach (FPSController player in playerInScene)
+            {
+                float dist = Vector2.Distance(transform.position, player.transform.position);
+                if (player == targetPlayer)
+                {
+                    if (dist > chaseRange)
+                    {
+                        targetPlayer = null;
+                      //  aim.SetBool("Move", false);
+                        rb.velocity = Vector2.zero;
+                    }
+                }
+                else if (dist < chaseRange)
+                {
+                    if (targetPlayer == null)
+                    {
+                        targetPlayer = player;
+                    }
+                }
+            }
+        }
+    }
+    [PunRPC]
+    public void TakeDamage(int attackerId, int damageAmount)
+    {
+        currentHP -= damageAmount;
+        curAttackerID = attackerId;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        FPSController player = GameManager.gamemanager.GetPlayer(curAttackerID);
+        PhotonNetwork.Instantiate(dead, transform.position, Quaternion.identity);
+        PhotonNetwork.Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, playerdetectRate);
+
     }
 }
--- Assets/Scripts/Zombie/Test/AI Zombie/State Machine Behaviours/AIStateMachineLink.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class AIStateMachineLink : StateMachineBehaviour 
-{
-	protected AIStateMachine _stateMachine;
-	public AIStateMachine stateMachine{ set{ _stateMachine = value;}}
-}
