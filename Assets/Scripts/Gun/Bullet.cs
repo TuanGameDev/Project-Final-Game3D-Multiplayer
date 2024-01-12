937cd75e822d3ac7b3992ffd640fbac3c38647f3
TuanGameDev <hoangtuan18203@gmail.com>
2024-01-12 16:21:07 +0700

    .

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Unity.VisualScripting;
 
-public class Bullet : MonoBehaviour
+public class Bullet : MonoBehaviourPun
 {
     [SerializeField] private float bulletSpeed = 20f;
-    private Rigidbody rb;
     [SerializeField] private float maxBulletDistance = 20f;
+    private Rigidbody rb;
     private float currentSpeed = 0f;
-
+    private int attackerId;
+    private bool isMine;
+    public int damage;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-
     private void Update()
     {
         rb.velocity = transform.forward * currentSpeed;
@@ -26,10 +29,24 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-    }
+        if (other.tag == "Zombie" && isMine)
+        {
+            Debug.Log("Va cham voi zombie");
 
+            AIZombie zombie = other.GetComponent<AIZombie>();
+            zombie.TakeDamage(attackerId,damage);
+
+            Destroy(gameObject);
+        }
+    }
+    [PunRPC]
     private void DestroyBullet()
     {
-        Destroy(gameObject);
+        Destroy(gameObject,2);
+    }
+    public void Initialized(int attackId, bool isMine)
+    {
+        this.attackerId = attackId;
+        this.isMine = isMine;
     }
 }
