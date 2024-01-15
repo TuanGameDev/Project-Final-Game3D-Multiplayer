073748d19ca8cfc9e789f341334c7b682ec9092e
mainaishin <114270989+mainaishin@users.noreply.github.com>
2024-01-15 13:06:48 +0700

    bullet impact enemy active blood

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿    using UnityEngine;
 using Photon.Pun;
 
 public class Bullet : MonoBehaviourPun
@@ -9,6 +9,8 @@ public class Bullet : MonoBehaviourPun
     private int attackerId;
     private bool isMine;
 
+
+    public GameObject bloodPrefab;
     private void Start()
     {
        // Invoke("DestroyBullet", destroyDelay);
@@ -24,11 +26,13 @@ public class Bullet : MonoBehaviourPun
         transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
     }
 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag== "Zombie" )
         {
             AIZombie zombie = other.GetComponent<AIZombie>();
+            ShowBloodEffect(other.transform.position);
             zombie.photonView.RPC("TakeDamage", RpcTarget.MasterClient,bulletDamage);
             DestroyBullet();
         }
@@ -41,5 +45,12 @@ public class Bullet : MonoBehaviourPun
     {
         this.attackerId = attackId;
         this.isMine = isMine;
+    }
+
+    private void ShowBloodEffect(Vector3 position)
+    {
+        GameObject blood = Instantiate(bloodPrefab, position, Quaternion.identity);
+        float particleDuration = blood.GetComponent<ParticleSystem>().main.duration;
+        Destroy(blood, particleDuration);
     }
 }
