6b0c4965cc5e1a3e32b45f7be061ebcf3c5f5b95
TuanGameDev <hoangtuan18203@gmail.com>
2024-05-09 16:50:42 +0700

    Update Mission Map 3, Update UI End Game, Update Boss

--- Assets/Scripts/Mission/MissionGenerator.cs
+++ Assets/Scripts/Mission/MissionGenerator.cs
@@ -1,25 +1,43 @@
-using Photon.Pun;
+﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MissionGenerator : MonoBehaviourPun
 {
     [Header("Mission Generator")]
+    [SerializeField] public string loadlevel;
+    [SerializeField] public float duration;
+    [SerializeField] public float spawnCollisionRadius;
+    [SerializeField] public Slider progressSlider;
+    [SerializeField] public TextMeshProUGUI notificationText;
     [SerializeField] public AudioSource footstepAudioSource;
     [SerializeField] public AudioClip footstepSounds;
+    [SerializeField] public GameObject uiWinGame;
     [SerializeField] public GameObject uiGenerator;
-    [SerializeField] public float spawnCollisionRadius;
+    [Header("Boss Mission 3")]
+    public GameObject bossPrefabs;
+    public Transform spawnBossPoint;
+    public float maxEnemies;
+    private List<GameObject> currentEnemies = new List<GameObject>();
     private PlayerController targetPlayer;
     private PlayerController[] playerInScene;
     private bool isInsideTrigger = false;
+    private bool isGeneratorRunning = false;
+    private bool isStartupSuccessful = false;
+    private void Start()
+    {
+        progressSlider.gameObject.SetActive(false);
+    }
     private void Update()
     {
         if (isInsideTrigger)
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                photonView.RPC("AudioGenerator", RpcTarget.All);
+                photonView.RPC("AudioGeneratorStart", RpcTarget.All);
             }
         }
         if (!PhotonNetwork.IsMasterClient)
@@ -35,8 +53,24 @@ public class MissionGenerator : MonoBehaviourPun
             if (distanceToPlayer < spawnCollisionRadius)
             {
                 targetPlayer = player;
+                SpawnBoss();
             }
         }
+    }
+    void SpawnBoss()
+    {
+        for (int x = currentEnemies.Count - 1; x >= 0; x--)
+        {
+            if (!currentEnemies[x])
+            {
+                currentEnemies.RemoveAt(x);
+            }
+        }
+
+        if (currentEnemies.Count >= maxEnemies)
+            return;
+        GameObject enemy = PhotonNetwork.Instantiate(bossPrefabs.name, spawnBossPoint.position, Quaternion.identity);
+        currentEnemies.Add(enemy);
     }
     private void OnTriggerEnter(Collider collision)
     {
@@ -47,10 +81,27 @@ public class MissionGenerator : MonoBehaviourPun
             {
                 uiGenerator.SetActive(true);
                 isInsideTrigger = true;
+                if (isGeneratorRunning)
+                {
+                    if (isStartupSuccessful)
+                    {
+                        notificationText.text = "Khởi động thành công!";
+                        notificationText.color = Color.green;
+                    }
+                    else
+                    {
+                        notificationText.text = "Đang khởi động...";
+                        notificationText.color = Color.yellow;
+                    }
+                }
+                else
+                {
+                    notificationText.text = "Nhấn Q để khởi động";
+                    notificationText.color = Color.red;
+                }
             }
         }
     }
-
     private void OnTriggerExit(Collider collision)
     {
         if (collision.CompareTag("Player"))
@@ -61,6 +112,7 @@ public class MissionGenerator : MonoBehaviourPun
                 uiGenerator.SetActive(false);
                 isInsideTrigger = false;
                 isInsideTrigger = false;
+                notificationText.text = "";
             }
         }
     }
@@ -70,12 +122,56 @@ public class MissionGenerator : MonoBehaviourPun
         Gizmos.DrawWireSphere(transform.position, spawnCollisionRadius);
     }
     [PunRPC]
-    void AudioGenerator()
+    void AudioGeneratorStart()
     {
         if (!footstepAudioSource.isPlaying)
         {
             footstepAudioSource.clip = footstepSounds;
             footstepAudioSource.Play();
         }
+        isGeneratorRunning = true;
+        notificationText.text = "Đang khởi động...";
+        notificationText.color = Color.yellow;
+        progressSlider.gameObject.SetActive(true);
+        StartCoroutine(UpdateSliderValue());
+    }
+    [PunRPC]
+    void AudioGeneratorStop()
+    {
+        footstepAudioSource.Stop();
+        progressSlider.gameObject.SetActive(false);
+    }
+    private IEnumerator UpdateSliderValue()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            float progress = elapsedTime / duration;
+            progressSlider.value = progress;
+
+            notificationText.text = string.Format("Đang khởi động... {0}%", Mathf.RoundToInt(progress * 100f));
+            notificationText.color = Color.yellow;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        progressSlider.value = 1f;
+        photonView.RPC("AudioGeneratorStop", RpcTarget.All);
+        notificationText.text = "Khởi động thành công! 100%";
+        notificationText.color = Color.green;
+
+        yield return new WaitForSeconds(5f);
+
+        uiWinGame.SetActive(true);
+        Time.timeScale = 0;
+
+        yield return new WaitForSecondsRealtime(11f);
+        Loadlevel();
+    }
+    public void Loadlevel()
+    {
+        PhotonNetwork.LoadLevel(loadlevel);
+        Time.timeScale = 1;
     }
 }
