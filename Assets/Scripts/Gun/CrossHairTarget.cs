872ac9aebd5c918df9f158a2eb269f94f0f498d5
TuanGameDev <hoangtuan18203@gmail.com>
2024-06-07 02:17:41 +0700

    Update Fix Gun tranform

--- Assets/Scripts/Gun/CrossHairTarget.cs
+++ Assets/Scripts/Gun/CrossHairTarget.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossHairTarget : MonoBehaviour
+public class CrossHairTarget : MonoBehaviourPunCallbacks
 {
     public Camera _cam;
     Ray _ray;
@@ -11,23 +12,34 @@ public class CrossHairTarget : MonoBehaviour
 
     void Update()
     {
-        _ray = _cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-
-        if (haveTarget)
+        if (photonView.IsMine)
         {
-            if (Physics.Raycast(_ray, out _hit))
+            _ray = _cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            if (haveTarget)
             {
-                transform.position = _hit.point;
+                if (Physics.Raycast(_ray, out _hit))
+                {
+                    transform.position = _hit.point;
+                    photonView.RPC("UpdateTargetPosition", RpcTarget.Others, _hit.point);
+                }
+                else
+                {
+                    transform.position = _ray.GetPoint(1000.0f);
+                    photonView.RPC("UpdateTargetPosition", RpcTarget.Others, _ray.GetPoint(1000.0f));
+                }
             }
             else
             {
                 transform.position = _ray.GetPoint(1000.0f);
+                photonView.RPC("UpdateTargetPosition", RpcTarget.Others, _ray.GetPoint(1000.0f));
             }
         }
-        else
-        {
-            transform.position = _ray.GetPoint(1000.0f);
-        }
+    }
 
+    [PunRPC]
+    void UpdateTargetPosition(Vector3 position)
+    {
+        transform.position = position;
     }
 }
